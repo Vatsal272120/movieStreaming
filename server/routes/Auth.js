cde8014604339ca7403cf72d8d2f41bc904204a2
vatsal <vatsalpatel1073@gmail.com>
2021-08-19 10:43:48 +0530

    auth-register

--- server/routes/Auth.js
+++ server/routes/Auth.js
@@ -1,18 +1,27 @@
 const Auth = require('express').Router();
+const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
-// Register user route.
+// Register user route. - post
 //  need to create a user with id an password (pass using the cryptojs lib) and save it to the database
 
+Auth.post('/register', async (req, res, next) => {
+  const { userName, email, password } = req.body;
 
+  const newUser = new User({
+    userName,
+    userEmail: email,
+    password: CryptoJS.AES.encrypt(password, process.env.SecretKey).toString(),
+  });
 
-
-
-
-
-
-
-
-
+  try {
+    const user = await newUser.save();
+    console.log(user);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 /* 
 User login route 
